dbaf6a17e7c7fad0abf06d61ca997ebb57ca1b29
Leonhard Markert <curiousleo@noreply.users.github.com>
2024-10-08 21:48:52 +0800

    Kinda works (in Go)

--- internal/rows2json.go
+++ internal/rows2json.go
@@ -2,51 +2,64 @@ package internal
 
 import (
 	"database/sql"
-	"encoding/json"
-	"reflect"
 )
 
-var (
-	stringTy = reflect.TypeFor[string]()
-	intTy    = reflect.TypeFor[int64]()
-	floatTy  = reflect.TypeFor[float64]()
-	boolTy   = reflect.TypeFor[bool]()
-)
+// var (
+// 	stringTy = reflect.TypeFor[string]()
+// 	intTy    = reflect.TypeFor[int64]()
+// 	floatTy  = reflect.TypeFor[float64]()
+// 	boolTy   = reflect.TypeFor[bool]()
+// )
 
-func RowsToJson(rows *sql.Rows) ([]byte, error) {
+func allNulls(columnTypes []*sql.ColumnType) []interface{} {
+	scanArgs := make([]interface{}, len(columnTypes))
+	for i := range columnTypes {
+		scanArgs[i] = new(interface{})
+		// scanTy := v.ScanType()
+		// if scanTy.ConvertibleTo(stringTy) {
+		// 	scanArgs[i] = new(sql.NullString)
+		// } else if scanTy.ConvertibleTo(floatTy) {
+		// 	scanArgs[i] = new(sql.NullFloat64)
+		// } else if scanTy.ConvertibleTo(intTy) {
+		// 	scanArgs[i] = new(sql.NullInt64)
+		// } else if scanTy.ConvertibleTo(boolTy) {
+		// 	scanArgs[i] = new(sql.NullBool)
+		// } else {
+		// 	scanArgs[i] = new(sql.NullString)
+		// }
+	}
+	return scanArgs
+}
+
+func RowsToJson(rows *sql.Rows) ([]Column, [][]any, error) {
 	columnTypes, err := rows.ColumnTypes()
-
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	count := len(columnTypes)
-	finalRows := []interface{}{}
+	n := len(columnTypes)
+	allNulls := allNulls(columnTypes)
+	data := [][]interface{}{}
 
 	for rows.Next() {
-		scanArgs := make([]interface{}, count)
-		for i, v := range columnTypes {
-			scanTy := v.ScanType()
-			if scanTy.ConvertibleTo(stringTy) {
-				scanArgs[i] = new(sql.NullString)
-			} else if scanTy.ConvertibleTo(floatTy) {
-				scanArgs[i] = new(sql.NullFloat64)
-			} else if scanTy.ConvertibleTo(intTy) {
-				scanArgs[i] = new(sql.NullInt64)
-			} else if scanTy.ConvertibleTo(boolTy) {
-				scanArgs[i] = new(sql.NullBool)
-			} else {
-				scanArgs[i] = new(sql.NullString)
-			}
-		}
-
+		var scanArgs []interface{}
+		scanArgs = append(scanArgs, allNulls...)
 		err := rows.Scan(scanArgs...)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 
-		finalRows = append(finalRows, scanArgs)
+		data = append(data, scanArgs)
 	}
 
-	return json.Marshal(finalRows)
+	columns := make([]Column, n)
+	for i := range columnTypes {
+		columnType := columnTypes[i]
+		columns[i] = Column{
+			Name: columnType.Name(),
+			Type: columnType.DatabaseTypeName(),
+		}
+	}
+
+	return columns, data, nil
 }
--- server.go
+++ server.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 	"trinoli/internal"
 
 	"github.com/labstack/echo/v4"
 	_ "github.com/marcboeker/go-duckdb"
+)
+
+const (
+	initialLimit = 1024
+	limitFactor  = 2
 )
 
 func queryResultsOfError(err error) internal.QueryResults {
@@ -35,27 +43,76 @@ func queryResultsOfError(err error) internal.QueryResults {
 	}
 }
 
-func query(db *sql.DB, c echo.Context, query string) error {
+func queryResultsOfSuccess(columns []internal.Column, data [][]any) internal.QueryResults {
+	return internal.QueryResults{
+		Id:               "",
+		InfoUri:          "",
+		PartialCancelUri: nil,
+		NextUri:          nil, // TODO: Generate nextUri
+		Columns:          columns,
+		Data:             data,
+		Stats:            internal.StatementStats{},
+		Error:            nil,
+		Warnings:         nil,
+		UpdateType:       nil,
+		UpdateCount:      nil,
+	}
+}
+
+func doQuery(db *sql.DB, c echo.Context, query string, limit int, offset int) error {
+	// TODO: Retrieve profiling info
+	// https://pkg.go.dev/github.com/marcboeker/go-duckdb@v1.8.1#ProfilingInfo
+	// duckdb.GetProfilingInfo(db)
+	// TODO: Is there a nicer way to do this?
+	query = fmt.Sprintf("SELECT * FROM (%s) LIMIT %d OFFSET %d", query, limit, offset)
+	println("query", query)
 	rows, err := db.QueryContext(c.Request().Context(), query)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, queryResultsOfError(err))
 	}
-	json, err := internal.RowsToJson(rows)
+	columns, data, err := internal.RowsToJson(rows)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, queryResultsOfError(err))
 	}
-	return c.JSONBlob(http.StatusOK, json)
+	return c.JSON(http.StatusOK, queryResultsOfSuccess(columns, data))
 }
 
 func main() {
-	db, err := sql.Open("duckdb", "?access_mode=READ_ONLY")
+	duckdbConfig := map[string]string{
+		"access_mode":                  "READ_ONLY",
+		"autoinstall_known_extensions": "false",
+		"autoload_known_extensions":    "false",
+		"enable_external_access":       "false",
+		"lock_configuration":           "true",
+	}
+	configQuery := []string{}
+	for k, v := range duckdbConfig {
+		configQuery = append(configQuery, k+"="+v)
+	}
+	duckdbFile := "/mnt/c/Users/Leo/Code/mastr-export/bnetza_mastr_2024-09.duckdb1"
+	dataSourceName := duckdbFile + "?" + strings.Join(configQuery, "&")
+	db, err := sql.Open("duckdb", dataSourceName)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 	e := echo.New()
 	e.POST("/v1/statement", func(c echo.Context) error {
-		return query(db, c, c.QueryParam("query"))
+		return doQuery(db, c, c.FormValue("query"), initialLimit, 0)
+	})
+	e.GET("/fetch", func(c echo.Context) error {
+		limit, err := strconv.Atoi(c.QueryParam("limit"))
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, queryResultsOfError(err))
+		}
+
+		offset, err := strconv.Atoi(c.QueryParam("offset"))
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, queryResultsOfError(err))
+		}
+
+		query := c.QueryParam("query")
+		return doQuery(db, c, query, limit, offset)
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
